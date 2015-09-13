0f537f7791b035fffb9645a3a9c9767c5e4ea0af
Leonardo Foderaro <leonardo.foderaro@gmail.com>
2015-09-14 01:05:42 +0200

    first working version

--- src/main/java/org/madeinjs/Application.java
+++ src/main/java/org/madeinjs/Application.java
@@ -1,5 +1,13 @@
 package org.madeinjs;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+import org.eclipse.aether.resolution.ArtifactResult;
+import org.madein.MadeIn;
+
 
 public class Application {
 
@@ -11,10 +19,12 @@ public class Application {
 		StringBuffer script = new StringBuffer();
 
 		script.append("/*\n\r");
-		script.append("@resolve org.apache.solr:solr-solrj:3.5.0 \n\r");
+		script.append("@resolve org.apache.solr:solr-solrj:5.3.0 \n\r");
+		script.append("@import java.lang.Thread \n\r");
 		script.append("@import org.apache.solr.common.SolrDocument \n\r");
 		script.append("*/ \n\r");
 		script.append("var doc = new SolrDocument();");
+		script.append("doc.setField('nome', 'leo');");
 		script.append("print(doc);");
 
 		System.out.println(engine.eval(script.toString()));
